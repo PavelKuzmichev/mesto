1b37ce9fadbfa56663dab7c383addef67fe30412
Pavel Kuzmichev <kuzpavel1985@yandex.ru>
2021-02-03 17:29:23 +0500

    8 работа 15

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import "./index.css";
-import Card from "../components/Card.js";
-import FormValidator from "../components/FormValidator.js";
-import { initialCards } from "../components/initialCards.js";
-import PopupWithImage from "../components/PopupWithImage.js";
-import PopupWithForm from "../components/PopupWithForm.js";
-import Section from "../components/Section.js";
-import UserInfo from "../components/UserInfo.js";
+import Card from "../scripts/Card.js";
+import FormValidator from "../scripts/FormValidator.js";
+import { initialCards } from "../scripts/initialCards.js";
+import PopupWithImage from "../scripts/PopupWithImage.js";
+import PopupWithForm from "../scripts/PopupWithForm.js";
+import Section from "../scripts/Section.js";
+import UserInfo from "../scripts/UserInfo.js";
 export const listContainerElement = ".elements";
 
 
